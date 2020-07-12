87beb579d006bb71ad66b3c7e82699429fc5ceed
Mark Stopka <mark.stopka@perlur.cloud>
2020-07-12 14:00:18 +0200

    Add FontAwesome

--- src/frontend/layouts/default.vue
+++ src/frontend/layouts/default.vue
@@ -2,6 +2,21 @@
   <nuxt/>
 </template>
 
+<script>
+export default {
+  head () {
+    return {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/all.min.css'
+        }
+      ]
+    }
+  }
+}
+</script>
+
 <style lang="scss">
   @import '~assets/scss/main.scss';
 </style>
