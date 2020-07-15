b5dada5ca54d4b36a99396f8aef1ba588e6f3750
Mark Stopka <mark.stopka@perlur.cloud>
2020-07-15 10:59:44 +0200

    Update header menu

--- src/frontend/components/header.vue
+++ src/frontend/components/header.vue
@@ -1,20 +1,27 @@
 <template>
   <header>
-    <nav class="navbar is-spaced" role="navigation" aria-label="dropdown main navigation">
+    <nav class="navbar is-spaced" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
-        <a href="/">
+        <a class="navbar-item" href="/">
           <img class="navbar-item logo" :src="TheStorageGuyLogoSVG" />
         </a>
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
-      <div class="navbar-end">
-        <span><a class="navbar-item is-hoverable title is-4" href="/">Overview</a></span>
-        <span><a class="navbar-item is-hoverable title is-4" href="/about-storage-guy/">About Storage Guy</a></span>
-        <a class="navbar-item" href="https://twitter.com/LiberalMark">
-        <i class="fab fa-twitter-square title"></i>
-        </a>
-        <a class="navbar-item" href="https://www.youtube.com/channel/UCp-grc_OkwssH4XcSU7Lo/">
-        <i class="fab fa-youtube-square title"></i>
-        </a>
+      <div class="navbar-menu">
+        <div class="navbar-end">
+          <span><a class="navbar-item is-hoverable title is-4" href="/">Overview</a></span>
+          <span><a class="navbar-item is-hoverable title is-4" href="/about-storage-guy/">About Storage Guy</a></span>
+          <a class="navbar-item" href="https://twitter.com/LiberalMark">
+          <i class="fab fa-twitter-square title"></i>
+          </a>
+          <a class="navbar-item" href="https://www.youtube.com/channel/UCp-grc_OkwssH4XcSU7Lo/">
+          <i class="fab fa-youtube-square title"></i>
+          </a>
+        </div>
       </div>
     </nav>
   </header>
