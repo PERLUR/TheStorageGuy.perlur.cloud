3c739c451db2a9f96001d3d1946d9f903084d52f
Mark Stopka <mark.stopka@perlur.cloud>
2020-07-15 03:33:11 +0200

    Move to columns based layout

--- src/frontend/pages/about-storage-guy/productivity-equipment.vue
+++ src/frontend/pages/about-storage-guy/productivity-equipment.vue
@@ -1,10 +1,16 @@
 <template>
-  <div id="site-content">
-    <h1>Productivity Equipment</h1>
-    <ul>
-      <li><strong>Computer</strong>: ASUS ZenBook S UX391UA</li>
-      <li><strong>External display</strong>: 28" Samsung U28E590</li>
-    </ul>
-    <h2>Productivity Equipment Wishlist</h2>
+  <div class="has-text-left" id="site-content">
+    <div class="columns">
+      <div class="column is-four-fifths">
+        <h1 class="title is-1">Productivity Equipment</h1>
+        <ul>
+          <li><strong>Computer</strong>: ASUS ZenBook S UX391UA</li>
+          <li><strong>External display</strong>: 28" Samsung U28E590</li>
+        </ul>
+      </div>
+      <div class="column">
+        <h2 class="title is-2">Productivity Equipment Wishlist</h2>
+      </div>
+    </div>
   </div>
 </template>
