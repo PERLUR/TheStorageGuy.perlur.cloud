3863d5ffc0c637067f400776cf5b8d0a98e4b815
Mark Stopka <mark.stopka@perlur.cloud>
2020-07-15 04:03:27 +0200

    Add items list

--- src/frontend/pages/about-storage-guy/productivity-equipment.vue
+++ src/frontend/pages/about-storage-guy/productivity-equipment.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="has-text-left" id="site-content">
     <div class="columns">
-      <div class="column is-four-fifths">
+      <div class="column is-two-thirds">
         <h1 class="title is-1">Productivity Equipment</h1>
         <ul>
-          <li><strong>Computer</strong>: ASUS ZenBook S UX391UA</li>
-          <li><strong>External display</strong>: 28" Samsung U28E590</li>
+          <li><strong>Computer</strong>: <a href="https://www.notebookcheck.net/Asus-ZenBook-S-UX391U-Core-i7-FHD-Laptop-Review.331490.0.html">ASUS ZenBook S UX391U</a></li>
+          <li><strong>External display</strong>: <a href="https://www.cnet.com/products/samsung-u28e590d-ue590-series-led-monitor-28/">28" Samsung U28E590</a></li>
+          <li><strong>Headset:</strong>: <a href="https://www.logitechg.com/en-us/products/gaming-audio/g933-7-1-surround-sound-gaming-headset.html">Logitech G933 Artemis Spectrum</a>/li>
+          <li><strong>Camera:</strong>: <a href="https://www.samsung.com/us/support/owners/product/camcorder-hmx-h204-h205-series">Samsung HMX-H204BP 1080p</a></li>
         </ul>
       </div>
       <div class="column">
