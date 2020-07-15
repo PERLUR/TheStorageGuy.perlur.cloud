9d7778ed85ddcfa578512ee49a145434e8fb7346
Mark Stopka <mark.stopka@perlur.cloud>
2020-07-15 03:21:29 +0200

    Fix template issues

--- src/frontend/pages/about-storage-guy/productivity-equipment.vue
+++ src/frontend/pages/about-storage-guy/productivity-equipment.vue
@@ -1,7 +1,10 @@
 <template>
-  <h1>Production Equipment</h1>
-  <ul>
-    <li><strong>Computer</strong>: ASUS ZenBook S UX391UA</li>
-    <li><strong>External display</strong>: 28" Samsung U28E590</li>
-  </ul>
+  <div id="site-content">
+    <h1>Productivity Equipment</h1>
+    <ul>
+      <li><strong>Computer</strong>: ASUS ZenBook S UX391UA</li>
+      <li><strong>External display</strong>: 28" Samsung U28E590</li>
+    </ul>
+    <h2>Productivity Equipment Wishlist</h2>
+  </div>
 </template>
