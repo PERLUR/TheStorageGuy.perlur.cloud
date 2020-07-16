2ed697734ac2c499737568d54239712aebadc267
Mark Stopka <mark.stopka@perlur.cloud>
2020-07-16 11:48:07 +0200

    Add customer NuxtJS eroor page

---
+++ src/frontend/layouts/error.vue
@@ -0,0 +1,7 @@
+<template>
+  <div id="site-content">
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <nuxt-link to="/">Home page</nuxt-link>
+  </div>
+</template>
