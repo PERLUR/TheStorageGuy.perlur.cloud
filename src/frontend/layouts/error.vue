3e384670d500e58079ea3428c781eba10ad886b5
Mark Stopka <mark.stopka@perlur.cloud>
2020-07-16 12:08:16 +0200

    Refactor NuxtJS error page

--- src/frontend/layouts/error.vue
+++ src/frontend/layouts/error.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="site-content">
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occurred</h1>
-    <nuxt-link to="/">Home page</nuxt-link>
+    <div v-if="error.statusCode === 404">
+      <h1 class="title is-1">404</h1>
+      <h2 class="subtitle">Page not found!</h2>
+    </div>
+    <div v-else>
+      <h1 class="title is-1">Unknown error</h1>
+      <h2 class="subtitle">An error occurred!</h2>
+    </div>
+    <nuxt-link to="/">Back to main page!</nuxt-link>
   </div>
 </template>
 
