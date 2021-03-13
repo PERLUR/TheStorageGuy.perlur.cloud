518af3490b3c1cb2d6219bd5711fe28423d30c88
mark-stopka <mark-stopka@users.noreply.github.com>
2021-03-13 14:50:55 +0000

    deploy: 70eb3fb2a00d52df4dfdbbed3a5cc48d58aa4ac1

---
+++ _nuxt/static/1615647052/about-storage-guy/productivity-equipment/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615647052",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout-storage-guy\u002Fproductivity-equipment",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b25b294c"}}}}(null));
