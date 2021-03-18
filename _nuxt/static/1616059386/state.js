bdc2dc9ea684e68903bdd8d387607341826a7ea9
mark-stopka <mark-stopka@users.noreply.github.com>
2021-03-18 09:23:10 +0000

    deploy: 18b4d355b896924ccb161be034e3107ee2c60924

---
+++ _nuxt/static/1616059386/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616059386",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fd5b0a52"}}}}(null,"\u002F"));
