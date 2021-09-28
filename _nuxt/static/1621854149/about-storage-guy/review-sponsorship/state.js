248c910245b1e8080d52c9bb8ffcbc514f573a87
mark-stopka <mark-stopka@users.noreply.github.com>
2021-09-28 23:03:08 +0000

    deploy: 747654acaa46c942ab9c22aed23e296538f974ea

--- _nuxt/static/1621854149/about-storage-guy/review-sponsorship/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621854149",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout-storage-guy\u002Freview-sponsorship",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4ec40526"}}}}(null));
