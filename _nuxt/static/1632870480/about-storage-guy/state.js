4c4939d339516a462d0bf569e638312007d6c008
mark-stopka <mark-stopka@users.noreply.github.com>
2021-09-28 23:14:24 +0000

    deploy: 4c6f296508e888c0d6372de3301403762693e42a

--- _nuxt/static/1632870480/about-storage-guy/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632870480",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout-storage-guy",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ddf33945"}}}}(null));
